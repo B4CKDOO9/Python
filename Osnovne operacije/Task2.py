8e79d5912fb05b47fb0b836dee17ea790e532cf5
The Mentor <smolecprofesional@gmail.com>
2024-05-14 23:39:56 +0200

    Among us task 2
    It is to late

---
+++ Osnovne operacije/Task2.py
@@ -0,0 +1,3 @@
+Godine = int(input('Unesite koji rodjendan je junak proslavio:'));
+God_proslavljenog_rod = int(input('Unesite godinu u kojoj je junak proslavio taj rodjendan:'));
+print(God_proslavljenog_rod - Godine);
---
+++ UUPIPJP/Task.py
@@ -0,0 +1,4 @@
+a = int (input());
+b = int (input());
+O = 2 * (a + b);
+print(O);
